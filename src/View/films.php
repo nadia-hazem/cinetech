47d0af4ce07c03068371a214e0f897eac950a841
nedh <nedh.design@gmail.com>
2023-05-17 12:12:17 +0200

    ajout de fichiers

---
+++ src/View/films.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Films</title>
+    <!-- Bootstrap 5.3.0 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="public/css/style.css">
+    <!-- Javascript -->
+    <!-- <script type="text/javascript" src="public/js/index.js"></script> -->
+
+</head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les Films</title>
+</head>
+<body>
+    
+    <main>
+        
+    </main>
+
+    <!-- Bootstrap js -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
